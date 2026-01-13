75e846de69f0ed28439f9e9da0f6e7d111eb9163
lucasximenes30 <lucasximenes177@gmail.com>
2026-01-13 09:47:10 -0300

    movendo o Container para seu próprio React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>
-            LOGO
-          </section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>
-            MENU
-          </section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>
-            FORMULÁRIO
-          </section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>
-            FOOTER
-          </section>
-        </div>
-      </div>
+       <Container>
+        <Heading>
+          Logo
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading>
+          Menu
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading>
+          Formulário
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading>
+          Footer
+        </Heading>
+      </Container>
     </> 
   );
 }
